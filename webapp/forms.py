0f119eb979bb06de06a15f727168cdd79deaeda2
rotich254 <rotichk254@gmail.com>
2024-12-21 10:10:01 +0300

    Initial commit

---
+++ webapp/forms.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import AuthenticationForm
+from django import forms
+from django.forms.widgets import PasswordInput, TextInput
+
+
+
+class CreateUserForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+        
+        
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(widget=TextInput())
+    password = forms.CharField(widget=PasswordInput())
